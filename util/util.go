47b358ece6f4fea366477c4dc4a4ee846e863e65
Jonas Karlsson <jonas@jonaskarlsson.dev>
2022-07-30 09:30:34 +0200

    Moved limited Split to util package

--- util/util.go
+++ util/util.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // ReadLines reads a file from the path and returns a slice containing each row read from the file.
@@ -20,6 +21,12 @@ func ReadLines(path string) ([]string, error) {
 		lines = append(lines, scanner.Text())
 	}
 	return lines, scanner.Err()
+}
+
+// Split separates a string into a slice of strings and returns the first and second entry.
+func Split(s string, sep string) (string, string) {
+	split := strings.Split(s, sep)
+	return split[0], split[1]
 }
 
 // ToIntArray takes a string slice and attemps to convert each string to an int.
