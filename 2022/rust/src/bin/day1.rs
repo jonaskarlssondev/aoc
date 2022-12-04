da25afffb7d6a3020a546fedc35c5636f53a87bf
Jonas Karlsson <jonas@jonaskarlsson.dev>
2022-12-04 16:46:23 +0100

    2022 day 2 part 2

--- 2022/rust/src/bin/day1.rs
+++ 2022/rust/src/bin/day1.rs
@@ -3,11 +3,14 @@ fn main() {
 
     let lines = input.split("\n\n");
    
-    let max: Option<u32> = lines
+    let mut inventories: Vec<u32> = lines
         .map(|line| line.split("\n")
              .flat_map(|n| n.parse::<u32>())
              .sum())
-        .max();
+        .collect();
 
-    println!("1: {:?}", max)
+    inventories.sort_by(|a, b| b.cmp(a));
+
+    println!("1: {:?}", inventories[0]);
+    println!("2: {:?}", inventories.iter().take(3).sum::<u32>());
 }
